87489c6f6e1810e46d83519bbe14f80b2bbf095e
BertKoor <bertkoor@gmail.com>
2020-06-16 20:57:10 +0200

    initial project from SpringBoot Initializr
    See https://start.spring.io
    maven jar project, java 11
    Added dependencies: Spring Web, Spring Boot Actuator and Apache Camel
    Removed the mvnw junk (who needs that)

---
+++ src/main/java/nl/ordina/bertkoor/camelcase/CamelCaseApplication.java
@@ -0,0 +1,13 @@
+package nl.ordina.bertkoor.camelcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelCaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/ordina/bertkoor/camelcase/CamelCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.ordina.bertkoor.camelcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
