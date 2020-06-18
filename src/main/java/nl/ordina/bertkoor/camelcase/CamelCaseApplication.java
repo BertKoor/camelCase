cdc1abf3e9ad84bc5fc38bfa6eb3d9b8959ff844
BertKoor <bertkoor@gmail.com>
2020-06-18 11:08:45 +0200

    reformatted code generated by Spring Initializr (tab -> space)

--- src/main/java/nl/ordina/bertkoor/camelcase/CamelCaseApplication.java
+++ src/main/java/nl/ordina/bertkoor/camelcase/CamelCaseApplication.java
@@ -10,47 +10,47 @@ import org.springframework.http.MediaType;
 @SpringBootApplication
 public class CamelCaseApplication extends RouteBuilder {
 
-	public static final String ERROR_URI = "direct:error";
+    public static final String ERROR_URI = "direct:error";
 
-	public static void main(String[] args) {
-		SpringApplication.run(CamelCaseApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CamelCaseApplication.class, args);
+    }
 
-	@Override
-	public void configure() {
-		restConfiguration()	// connect the Apache Camel Rest component to the Spring Boot servlet
-				.component("servlet");
+    @Override
+    public void configure() {
+        restConfiguration()    // connect the Apache Camel Rest component to the Spring Boot servlet
+                .component("servlet");
 
-		configureErrorHandling();
-		configureEndpointPing();
-		configureEndpointIdInternal();
-	}
+        configureErrorHandling();
+        configureEndpointPing();
+        configureEndpointIdInternal();
+    }
 
-	private void configureErrorHandling() {
-		onException(IllegalArgumentException.class)
-				.routeId("badRequest")
-				.handled(true)
-				.setHeader(Exchange.HTTP_RESPONSE_CODE, constant(HttpStatus.BAD_REQUEST.value()))
-				.to(ERROR_URI);
+    private void configureErrorHandling() {
+        onException(IllegalArgumentException.class)
+                .routeId("badRequest")
+                .handled(true)
+                .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(HttpStatus.BAD_REQUEST.value()))
+                .to(ERROR_URI);
 
-		from(ERROR_URI) // other routes can continue here
-				.setHeader(Exchange.CONTENT_TYPE, constant(MediaType.APPLICATION_JSON_VALUE))
-				.transform().simple("{\"message\":\"${exception.message}\"}");
-	}
+        from(ERROR_URI) // other routes can continue here
+                .setHeader(Exchange.CONTENT_TYPE, constant(MediaType.APPLICATION_JSON_VALUE))
+                .transform().simple("{\"message\":\"${exception.message}\"}");
+    }
 
-	private void configureEndpointPing() {
-		rest().get("/ping") // be aware this is within the servlet's context path set in application.properties
-				.route().routeId("ping")
-				.setHeader(Exchange.CONTENT_TYPE, constant(MediaType.TEXT_PLAIN_VALUE))
-				.setBody(constant("pong"));
-	}
+    private void configureEndpointPing() {
+        rest().get("/ping") // be aware this is within the servlet's context path set in application.properties
+                .route().routeId("ping")
+                .setHeader(Exchange.CONTENT_TYPE, constant(MediaType.TEXT_PLAIN_VALUE))
+                .setBody(constant("pong"));
+    }
 
-	private void configureEndpointIdInternal() {
-		rest().get("/camel/{id}/internal")
-				.route().routeId("id-internal")
-				.to(TransformIdBean.URI)
-				.setHeader(Exchange.CONTENT_TYPE, constant(MediaType.TEXT_PLAIN_VALUE))
-				.setBody(simple("${header.id}"));
-	}
+    private void configureEndpointIdInternal() {
+        rest().get("/camel/{id}/internal")
+                .route().routeId("id-internal")
+                .to(TransformIdBean.URI)
+                .setHeader(Exchange.CONTENT_TYPE, constant(MediaType.TEXT_PLAIN_VALUE))
+                .setBody(simple("${header.id}"));
+    }
 
 }
--- src/test/java/nl/ordina/bertkoor/camelcase/CamelCaseApplicationTests.java
+++ src/test/java/nl/ordina/bertkoor/camelcase/CamelCaseApplicationTests.java
@@ -14,62 +14,62 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 class CamelCaseApplicationTests {
 
-	@LocalServerPort
-	private int localServerPort;
+    @LocalServerPort
+    private int localServerPort;
 
-	@BeforeEach
-	void setup() {
-		RestAssured.port = localServerPort;
-	}
+    @BeforeEach
+    void setup() {
+        RestAssured.port = localServerPort;
+    }
 
-	@Test
-	void welcomePageLoads() {
-		RestAssured.when().get("/")
-				.then().statusCode(HttpStatus.SC_OK)
-				.contentType(ContentType.HTML)
-				.body("html.body.h1", equalTo("camelCase"));
-	}
+    @Test
+    void welcomePageLoads() {
+        RestAssured.when().get("/")
+                .then().statusCode(HttpStatus.SC_OK)
+                .contentType(ContentType.HTML)
+                .body("html.body.h1", equalTo("camelCase"));
+    }
 
-	@Test
-	void actuatorInfoEndpointLoads() {
-		RestAssured.when().get("/actuator/info")
-				.then().statusCode(HttpStatus.SC_OK)
-				.contentType(ContentType.JSON)
-				.body("appName", equalTo("camelCase"))
-				.body("'camel.name'", equalTo("camelCaseContext"));
-	}
+    @Test
+    void actuatorInfoEndpointLoads() {
+        RestAssured.when().get("/actuator/info")
+                .then().statusCode(HttpStatus.SC_OK)
+                .contentType(ContentType.JSON)
+                .body("appName", equalTo("camelCase"))
+                .body("'camel.name'", equalTo("camelCaseContext"));
+    }
 
-	@Test
-	void actuatorHealthEndpointLoads() {
-		RestAssured.when().get("/actuator/health")
-				.then().statusCode(HttpStatus.SC_OK)
-				.contentType(ContentType.JSON)
-				.body("status", equalTo("UP"))
-				.body("components.camel.status", equalTo("UP"));
-	}
+    @Test
+    void actuatorHealthEndpointLoads() {
+        RestAssured.when().get("/actuator/health")
+                .then().statusCode(HttpStatus.SC_OK)
+                .contentType(ContentType.JSON)
+                .body("status", equalTo("UP"))
+                .body("components.camel.status", equalTo("UP"));
+    }
 
-	@Test
-	void pingPongTest() {
-		RestAssured.when().get("/my-api/ping")
-				.then().statusCode(HttpStatus.SC_OK)
-				.contentType(ContentType.TEXT)
-				.content(equalTo("pong"));
-	}
+    @Test
+    void pingPongTest() {
+        RestAssured.when().get("/my-api/ping")
+                .then().statusCode(HttpStatus.SC_OK)
+                .contentType(ContentType.TEXT)
+                .content(equalTo("pong"));
+    }
 
-	@Test
-	void testIdInternalEndpoint_ok() {
-		RestAssured.when().get("/my-api/camel/00000a/internal")
-				.then().statusCode(HttpStatus.SC_OK)
-				.contentType(ContentType.TEXT)
-				.content(equalTo("10"));
-	}
+    @Test
+    void testIdInternalEndpoint_ok() {
+        RestAssured.when().get("/my-api/camel/00000a/internal")
+                .then().statusCode(HttpStatus.SC_OK)
+                .contentType(ContentType.TEXT)
+                .content(equalTo("10"));
+    }
 
-	@Test
-	void testIdInternalEndpoint_error() {
-		RestAssured.when().get("/my-api/camel/x/internal")
-				.then().statusCode(HttpStatus.SC_BAD_REQUEST)
-				.contentType(ContentType.JSON)
-				.body("message", equalTo("Invalid license ID."));
-	}
+    @Test
+    void testIdInternalEndpoint_error() {
+        RestAssured.when().get("/my-api/camel/x/internal")
+                .then().statusCode(HttpStatus.SC_BAD_REQUEST)
+                .contentType(ContentType.JSON)
+                .body("message", equalTo("Invalid license ID."));
+    }
 
 }
